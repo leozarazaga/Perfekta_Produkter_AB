bc3a6b7de32078df51022066caa23af3c6aad7fd
CaptainMorgan1992 <kimberly.morgan1992@outlook.com>
2023-09-06 13:20:18 +0200

    Added functionality to add Intern

--- src/main/java/org/example/InternMenu.java
+++ src/main/java/org/example/InternMenu.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 import static org.example.Intern.internList;
@@ -16,7 +18,7 @@ public class InternMenu {
             System.out.println(internList);
             break;
         case 2:
-            //addIntern();
+            addIntern();
             break;
         case 3:
             //removeIntern();
@@ -30,4 +32,24 @@ public class InternMenu {
     }
 
     }
+    public static void addIntern() {
+        System.out.println("Please enter ID of the new intern:");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Please enter gender of the intern (male/female):");
+        String gender = scanner.nextLine();
+        System.out.println("Please enter name of the intern (Firstname Surname):");
+        String name = scanner.nextLine();
+        System.out.println("Please enter the date when the intern will end their internship: (yyyy-mm-dd)");
+        String date = scanner.nextLine();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate endDate = LocalDate.parse(date, dateFormatter);
+        System.out.println("Please enter quitmessage for the intern: ");
+        String quitMessage = scanner.nextLine();
+
+        Intern intern = new Intern(id, gender, name, endDate, quitMessage);
+        Intern.internList.add(intern);
+
+    }
+
 }
